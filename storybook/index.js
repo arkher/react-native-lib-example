e6c79399f7b334f616e9afdb5b9dbc30a562d880
Paulo Gomes <paulo.rgomes22@gmail.com>
2021-09-22 09:30:52 -0300

    chore(storybook): ajustando configuração do expo

--- storybook/index.js
+++ storybook/index.js
@@ -1,6 +1,3 @@
-// if you use expo remove this line
-import { AppRegistry } from 'react-native';
-
 import { getStorybookUI, configure, addDecorator } from '@storybook/react-native';
 import { withKnobs } from '@storybook/addon-knobs';
 
@@ -16,10 +13,8 @@ configure(() => {
 
 // Refer to https://github.com/storybookjs/storybook/tree/master/app/react-native#start-command-parameters
 // To find allowed options for getStorybookUI
-const StorybookUIRoot = getStorybookUI({});
-
-// If you are using React Native vanilla and after installation you don't see your app name here, write it manually.
-// If you use Expo you should remove this line.
-AppRegistry.registerComponent('%APP_NAME%', () => StorybookUIRoot);
+const StorybookUIRoot = getStorybookUI({
+  asyncStorage: null,
+});
 
 export default StorybookUIRoot;
